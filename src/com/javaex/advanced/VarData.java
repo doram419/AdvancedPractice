9f243d0b3bc89a592822f818c71547190ad2c499
하이미디어학원 <@DESKTOP-PTAJFQA>
2024-03-19 12:59:53 +0900

    ver 0.1 원시적인 자료형 학습 프로그램

---
+++ src/com/javaex/advanced/VarData.java
@@ -0,0 +1,77 @@
+package com.javaex.advanced;
+
+public class VarData {
+	// 변수 정보
+	// 자료형 / 키워드 / 크기 / 표현 범위 / 예제
+	private String mType;
+	private String mKeyword;
+	private String mByte;
+	private String mRange; 
+	private String mExample;
+	
+	// 생성자를 만들면 조립이 어렵지만 강제성을 띄게 하자.
+	public VarData(String pType, String pKeyword, String pByte, 
+			String pRange, String pExample) {
+		setType(pType);
+		setKeyword(pKeyword);
+		setByte(pByte);
+		setRange(pRange);
+		setExample(pExample);
+	}
+	
+	public void printInfo()
+	{
+		System.out.println("자료형 : " + getType() + "\n키워드 : " +
+				getKeyword() + "\n크기 : " + getByte() + "\n표현 범위 : " +
+				getRange() + "\n사용 예 : " + getExample() + "\n");
+	}
+	
+	private String getType()
+	{
+		return mType;
+	}
+	
+	private void setType(String pType)
+	{
+		mType = pType;
+	}
+	
+	private String getKeyword()
+	{
+		return mKeyword;
+	}
+	
+	private void setKeyword(String pKeyword)
+	{
+		mKeyword = pKeyword;
+	}
+
+	private String getByte()
+	{
+		return mByte;
+	}
+	
+	private void setByte(String pByte)
+	{
+		mByte = pByte;
+	}
+	
+	private String getRange()
+	{
+		return mRange;
+	}
+	
+	private void setRange(String pRange)
+	{
+		mRange = pRange;
+	}
+	
+	private String getExample()
+	{
+		return mExample;
+	}
+	
+	private void setExample(String pExample) {
+		mExample = pExample;
+	}
+}
---
+++ src/com/javaex/advanced/varInfoPrint.java
@@ -0,0 +1,140 @@
+package com.javaex.advanced;
+
+import java.util.Scanner;
+import com.javaex.advanced.VarData;
+
+public class varInfoPrint {
+	// 변수형을 선택하면 크기와 최댓값을 알려주는 프로그램
+	// ver 0.1
+	
+	public static void main(String[] args) {
+		VarData cData1 = new VarData("논리형", "boolean",
+				"1byte", "true OR false(0과 1이 아니다)", 
+				"boolean isFun = true;");
+		VarData cData2 = new VarData("문자형", "char",
+				"2byte", "모든 유니코드 문자\n(\\u0000~\\uFFFF, 0~65535)", 
+				"char c = 'f';");
+		VarData cData3 = new VarData("정수형", "byte",
+				"1byte", "-128 ~ 127", 
+				"byte b = 89;");
+		VarData cData4 = new VarData("정수형", "short",
+				"2byte", "-32,768 ~ 32,767 ", 
+				"short s = 32760;");
+		VarData cData5 = new VarData("정수형", "int",
+				"4byte", "-2,147,483,648 ~ 2,147,483,647", 
+				"int x = 59;");
+		VarData cData6 = new VarData("정수형", "long",
+				"8byte", "-9,223,372,036,854,775,808 ~ "
+						+ "9,223,372,036,854,775,807", 
+				"long big = 345678912345L");
+		VarData cData7 = new VarData("실수형", "float",
+				"4byte", "-3.4E38 ~ 3.4E38 ", 
+				"float f = 32.5F;");
+		VarData cData8 = new VarData("실수형", "double",
+				"8byte", "-1.7E308 ~ 1.7E308", 
+				"double d = 2.3e10;");	
+		
+		boolean isContinue = true;
+		int userInput = -1;
+		
+		init();
+		
+		// 프로그램 소개(Intro)
+		intro();
+		
+		// 스캐너 열기
+		Scanner sc = new Scanner(System.in);	
+		
+		while (isContinue) {
+			//메뉴 출력
+			menu();
+			
+			// 유저입력 받기
+			System.out.print("입력 > ");
+			userInput = sc.nextInt();
+			
+			// 유저가 0 선택시 종료 메서드 호출
+			if(userInput == 0)
+			{
+				isContinue = end();
+			}
+			// 유저가 1~8 선택시 정보 출력 메서드 호출
+			// 유저 입력에 맞춘 정보 출력
+			else if(userInput >= 1 && userInput <= 8)
+			{
+				switch (userInput) {
+				case 1 -> {
+					cData1.printInfo();
+				}
+				case 2 -> {
+					cData2.printInfo();
+				}
+				case 3 -> {
+					cData3.printInfo();
+				}
+				case 4 -> {
+					cData4.printInfo();
+				}
+				case 5 -> {
+					cData5.printInfo();
+				}
+				case 6 -> {
+					cData6.printInfo();
+				}
+				case 7 -> {
+					cData7.printInfo();
+				}
+				case 8 -> {
+					cData8.printInfo();
+				}
+				default ->
+					throw new IllegalArgumentException("Unexpected value: " + userInput);
+				}
+			}
+			// 유저가 오입력시 에러 메서드 호출
+			else
+			{
+				error();
+			}
+		}
+		
+		// 스캐너 닫기
+		sc.close();
+	}
+	
+	// 초기화
+	private static void init() {
+		// 깔끔하게 init로 빼고 싶지만
+		// 데이터 매니저 만들고 파일 입출력까지 생각해버리니까 머리가 복잡하다
+		// 일단 대충하자.
+		System.out.println("초기화 중");
+	}
+	
+	// 프로그램을 구동하면 나오는 설명
+	// 변수 학습 프로그램
+	private static void intro() {
+		System.out.println("========================================");
+		System.out.println("  변수 타입을 선택하면 정보를 알려주는 프로그램");
+		System.out.println("========================================");
+	}
+	
+	// 메뉴 메서드
+	private static void menu() {
+		System.out.println("논리형 : 1. boolean\n"
+				+ "문자형 : 2. char\n"
+				+ "정수형 : 3. byte\t4. short\t5. int\t6. long\n"
+				+ "실수형 : 7. float\t8. double\t"
+				+ "[ 종료 : (0) ]");
+	}
+	
+	// 프로그램 종료 메서드
+	private static boolean end() {
+		System.out.println("프로그램을 종료합니다.");
+		return false;
+	}
+
+	// 오입력시 처리 메서드
+	private static void error() {
+		System.out.println("ERORR : 잘못된 번호입니다!\n다시 입력해주세요");
+	}
+}
